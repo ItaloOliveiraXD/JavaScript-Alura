43ecf579774c765820f5c7aa34e4d219805c7fb8
ítalo.oliveira <italooliveira-xd@hotmail.com>
2023-06-05 10:58:01 -0300

    Projeto finalizado.

--- -robotron-2000-projeto_inicial/js/main.js
+++ -robotron-2000-projeto_inicial/js/main.js
@@ -1,23 +1,58 @@
-const braco = document.querySelector('#braco');
-const somar = document.querySelector('#somar');
-const subtrair = document.querySelector('#subtrair');
+const controle = document.querySelectorAll('[data-controle]');
+const estatisticas = document.querySelectorAll('[data-estatistica]');
 
-const controle = document.querySelectorAll('.controle-ajuste');
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
 
-controle.forEach( (elemento) => {
-    elemento.addEventListener('click', (evento) => {
-        manipulaDados(evento.target.textContent);
-    });
-} );
-
-somar.addEventListener("click", () => manipulaDados('somar'));
-
-subtrair.addEventListener("click", () => manipulaDados('subtrair'));
-
-function manipulaDados(comando) {
-    if (comando === '+') {
-        braco.value = parseInt(braco.value) + 1;
-    } else {
-        braco.value = parseInt(braco.value) - 1;
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas": {
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
     }
+}
+
+controle.forEach((elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
+        atualizaEstatisticas(evento.target.dataset.peca);
+    });
+});
+
+function manipulaDados(comando, controle) {
+    const peca = controle.querySelector('[data-contador]');
+    if (comando === '+') {
+        peca.value = parseInt(peca.value) + 1;
+    } else {
+        peca.value = parseInt(peca.value) - 1;
+    }
+}
+
+function atualizaEstatisticas(peca) {
+    estatisticas.forEach((elemento) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+    })
 }
