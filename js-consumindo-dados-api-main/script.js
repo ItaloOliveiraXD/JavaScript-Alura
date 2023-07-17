6f3d9c14d5ea1b81cd4a7fbb3cccbd5a476d323d
Italo.Oliveira <italooliveira-xd@hotmail.com>
2023-07-17 11:23:05 -0300

    conclusão do curso consumindo dados api JS.

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -1,11 +1,30 @@
-let consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resp => resp.json())
-    .then(resp => {
-        if (resp.erro) {
-            throw Error('Esse CEP não existe!')
-        } else {
-            console.log(resp)
-        }
-    })
-    .catch(erro => console.log(erro))
-    .finally(msg => console.log('Processamento concluído!'));
+const cep = document.getElementById('cep');
+const logradouro = document.getElementById('endereco');
+const cidade = document.getElementById('cidade');
+const estado = document.getElementById('estado');
+const bairro = document.getElementById('bairro');
+const mensagemErro = document.getElementById('erro');
+
+cep.addEventListener('focusout', () => buscaEndereco(cep.value));
+
+async function buscaEndereco(cep) {
+    mensagemErro.innerHTML = ' ';
+    try {
+        let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        let cepJSON = await consultaCEP.json()
+
+        logradouro.value = cepJSON.logradouro;
+        cidade.value = cepJSON.localidade;
+        estado.value = cepJSON.uf;
+        bairro.value = cepJSON.bairro;
+
+        console.log(cepJSON);
+    } catch(erro) {
+        mensagemErro.innerHTML = `<p>Cep inválido! Tente novamente.</p>`
+        logradouro.value = '';
+        cidade.value = '';
+        estado.value = '';
+        bairro.value = '';
+        console.log('Cep não existente!')
+    }
+}
